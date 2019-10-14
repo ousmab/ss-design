7a3e801caf00315181fa1a4952cd17d48e6f04d0
ousmane <aboousmane@gmail.com>
2019-10-14 18:19:51 +0100

    ajout du dropdown d'options sur un article

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,11 @@
 $(function(){
+
+	/*fixer le menu de gauche*/
 	$("#salers-menu-items > .card").sticky({topSpacing:"4.5rem"});
+
+
+	$('#dropdown-btn').onclick(function() {
+        $(this).dropdown()
+      })
+
 } )
