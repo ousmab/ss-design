16e74e45e4a05f73ac5c87b784c4aa83cf11c685
aboubakar <aboousmane@gmail.com>
2019-10-16 14:48:03 +0100

    refoctoring du menu du haut (nav bar)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,9 @@
 $(function(){
 
-	/*fixer le menu de gauche*/
+	/*fixer le menu de gauche*/ 
 	$("#salers-menu-items > .card").sticky({topSpacing:"4.5rem"});
+
+	/*$("#salers-others-informations").sticky({topSpacing:"4.5rem"});*/
 
 
 	$('#dropdown-btn').onclick(function() {
